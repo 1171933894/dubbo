6c03fb670c76b99a7529a1cd4829a2b5d4a87c75
heyuanxin3 <heyuanxin3@jd.com>
2021-06-26 21:43:46 +0800

    c

--- dubbo-rpc/dubbo-rpc-api/src/main/java/com/alibaba/dubbo/rpc/service/GenericService.java
+++ dubbo-rpc/dubbo-rpc-api/src/main/java/com/alibaba/dubbo/rpc/service/GenericService.java
@@ -21,6 +21,17 @@ package com.alibaba.dubbo.rpc.service;
  *
  * @export
  */
+/**
+ * 在 Spring 配置申明 generic="true"：
+ * <dubbo:reference id="demoService"  interface="com.alibaba.dubbo.demo.DemoService" generic="true" />
+ *
+ * interface 配置项，泛化引用的服务接口。通过该配置，可以从注册中心，获取到所有该服务的提供方的地址。
+ * generic 配置项，默认为 false ，不使用配置项。目前有三种配置项的值，开启泛化引用的功能：
+ *      generic=true ，使用 com.alibaba.dubbo.common.utils.PojoUtils ，实现 POJO <=> Map 的互转。
+ *      generic=nativejava ，使用 com.alibaba.dubbo.common.serialize.support.nativejava.NativeJavaSerialization ，实现 POJO <=> byte[] 的互转。
+ *      generic=bean ，使用 com.alibaba.dubbo.common.beanutil.JavaBeanSerializeUtil ，实现 POJO <=> JavaBeanDescriptor 的互转。
+ * 总的来说，三种方式的差异，在于使用互转( 序列化和反序列化 )的方式不同。未来如果我们有需要，完成可以实现 generic=json ，使用 FastJSON 来序列化和反序列化。
+ */
 
 /**
  * 注意：一个泛化引用，只对应一个服务实现
